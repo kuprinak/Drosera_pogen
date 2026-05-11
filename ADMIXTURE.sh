0f24b0f54d41a2e1191040c78b69b67c1a5a24d0
Kristina Kuprina <133106535+kuprinak@users.noreply.github.com>
2026-05-11 18:33:49 +0200

    ADMIXTURE.sh

---
+++ ADMIXTURE.sh
@@ -0,0 +1,34 @@
+## install bcftools, plink and admixture
+```{bash}
+bcftools view -m2 -M2 -v snps md6ct95ms200.vcf -Oz -o md6ct95ms200.biallelic.vcf.gz
+bcftools index md6ct95ms200.biallelic.vcf.gz
+```
+# Create a numeric mapping
+```{bash}
+plink --vcf md6ct95ms200.biallelic.vcf.gz --make-bed --out md6ct95ms200_biallelic --allow-extra-chr 
+
+awk '{$1=1; print $0}' md6ct95ms200_biallelic.bim > md6ct95ms200_biallelic_numeric.bim
+
+plink --bfile md6ct95ms200_biallelic \
+      --make-bed \
+      --bim md6ct95ms200_biallelic_numeric.bim \
+      --fam md6ct95ms200_biallelic.fam \
+      --out md6ct95ms200_numeric
+
+plink --bfile md6ct95ms200_numeric \
+      --geno 0.9999 \
+      --make-bed \
+      --out md6ct95ms200_numeric
+```
+
+
+## Run ADMIXTURE
+```{bash}
+source activate admixture
+
+for K in {1..20}; do
+    admixture --cv=10 md6ct95ms200_numeric.bed $K | tee log${K}.out
+done
+
+grep "CV error" log*.out | sed -E 's/.*\(K=([0-9]+)\): ([0-9.]+)/\1 \2/' > cv_errors.txt
+```
